0d2b70ef54cb69c9ab60fb0210cff3138ed4891e
SaulRet <sau1585@hotmail.com>
2024-08-11 21:39:01 -0600

    Movimiento de modulo
    Se agrega mecanico a header

--- web-page-lubricentro/src/components/Login/header.js
+++ web-page-lubricentro/src/components/Login/header.js
@@ -2,7 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import NavDropdown from "react-bootstrap/NavDropdown";
 import Navbar from "react-bootstrap/Navbar";
 
 export default function AppHeader() {
@@ -21,13 +21,22 @@ export default function AppHeader() {
             <Nav.Link as={NavLink} to="/admin">
               Administrador
             </Nav.Link>
+            <Nav.Link as={NavLink} to="/mechanic">
+              Mecánico
+            </Nav.Link>
             <Nav.Link as={NavLink} to="/feedback">
               Retroalimentación
             </Nav.Link>
             <NavDropdown title="Contabilidad" id="basic-nav-dropdown">
-                            <NavDropdown.Item as={NavLink} to="/ingresos">Ingresos</NavDropdown.Item>
-                            <NavDropdown.Item as={NavLink} to="/egresos">Egresos</NavDropdown.Item>
-                            <NavDropdown.Item as={NavLink} to="/reportes-financieros">Reportes Financieros</NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/ingresos">
+                Ingresos
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/egresos">
+                Egresos
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/reportes-financieros">
+                Reportes Financieros
+              </NavDropdown.Item>
             </NavDropdown>
             <Nav.Link as={NavLink} to="/comentarios-valoraciones">
               Opiniones
--- web-page-lubricentro/src/pages/MechanicPage.jsx
+++ web-page-lubricentro/src/pages/MechanicPage.jsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import OrdenList from '../components/orden/OrdenList';
-import OrdenDetail from '../components/orden/OrdenDetail';
-import { Route, Routes, Link, useResolvedPath } from 'react-router-dom';
+import React from "react";
+import MechanicOrdenList from "../components/mecanico/MechanicOrdenList";
 
 const MechanicPage = () => {
-  const resolvedPath = useResolvedPath();
-  const baseUrl = resolvedPath.pathname;
-
   return (
     <div>
-      <h1>Mecánico</h1>
-      <nav>
-        <ul>
-          <li><Link to={`${baseUrl}/mis-ordenes`}>Mis Órdenes</Link></li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="mis-ordenes" element={<OrdenList />} />
-        <Route path=":id" element={<OrdenDetail />} />
-      </Routes>
+      <MechanicOrdenList />
     </div>
   );
 };
