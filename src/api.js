6ef7b0030b3e29b8ab9d5284098287027d82c2a3
SaulRet <sretana30764@ufide.ac.cr>
2024-11-19 16:22:54 -0600

    actualizar el endpoint url por azure y mostrar u ocultar menus si esta logueado o no

--- src/api.js
+++ src/api.js
@@ -1,6 +1,5 @@
-// src/api.js
-
-const BASE_URL = "https://localhost:7180/api";
+const BASE_URL =
+  "https://apirymlubricentro-dddjebcxhyf6hse7.centralus-01.azurewebsites.net/api";
 
 export const API_ROUTES = {
   users: `${BASE_URL}/Usuarios`,
--- src/components/HeaderFooter/header.js
+++ src/components/HeaderFooter/header.js
@@ -1,4 +1,3 @@
-// src/components/AppHeader.js
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { NavLink, useNavigate } from "react-router-dom";
@@ -15,12 +14,11 @@ export default function AppHeader() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  // Obtener el estado de autenticación desde Redux
   const { isAuthenticated, user } = useSelector((state) => state.loginState);
 
   const handleLogout = () => {
     dispatch(logout());
-    navigate("/Inicio");
+    navigate("/Inicio", { replace: true });
   };
 
   return (
@@ -32,75 +30,82 @@ export default function AppHeader() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <NavDropdown title="Administrador" id="admin-nav-dropdown">
-              <NavDropdown.Item as={NavLink} to="/lista-ordenes">
-                Lista de Órdenes
-              </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/reporte-ordenes">
-                Reportes de Órdenes
-              </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/marketing">
-                Marketing
-              </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/ListaUsuario">
-                Lista Usuario
-              </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/calendario">
-                Calendario
-              </NavDropdown.Item>
-            </NavDropdown>
-
-            <NavDropdown title="Mecánico" id="mechanic-nav-dropdown">
-              <NavDropdown.Item as={NavLink} to="/mis-ordenes">
-                Mis Órdenes
-              </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/reporte-retroalimentacion">
-                Reportes de Retroalimentación
-              </NavDropdown.Item>
-            </NavDropdown>
-
-            <NavDropdown title="Contabilidad" id="accounting-nav-dropdown">
-              <NavDropdown.Item as={NavLink} to="/ingresos">
-                Ingresos
-              </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/egresos">
-                Egresos
-              </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/reportes-financieros">
-                Reportes Financieros
-              </NavDropdown.Item>
-              <Nav.Link as={NavLink} to="/calendario">
-                Disponibilidad Citas
-              </Nav.Link>
-            </NavDropdown>
-
-            <Nav.Link as={NavLink} to="/comentarios-valoraciones">
-              Opiniones
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/inventarios">
-              Inventarios
-            </Nav.Link>
-
             {isAuthenticated ? (
-              <NavDropdown title={user.nombre} id="user-nav-dropdown">
-                <NavDropdown.Item as={NavLink} to="/Perfil">
-                  Perfil
-                </NavDropdown.Item>
-                <NavDropdown.Item onClick={handleLogout}>
-                  Cerrar Sesión
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item disabled>Rol: {user.rol}</NavDropdown.Item>
-              </NavDropdown>
+              <>
+                <NavDropdown title="Administrador" id="admin-nav-dropdown">
+                  <NavDropdown.Item as={NavLink} to="/lista-ordenes">
+                    Lista de Órdenes
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={NavLink} to="/reporte-ordenes">
+                    Reportes de Órdenes
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={NavLink} to="/marketing">
+                    Marketing
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={NavLink} to="/ListaUsuario">
+                    Lista Usuario
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={NavLink} to="/calendario">
+                    Calendario
+                  </NavDropdown.Item>
+                </NavDropdown>
+
+                <NavDropdown title="Mecánico" id="mechanic-nav-dropdown">
+                  <NavDropdown.Item as={NavLink} to="/mis-ordenes">
+                    Mis Órdenes
+                  </NavDropdown.Item>
+                  <NavDropdown.Item
+                    as={NavLink}
+                    to="/reporte-retroalimentacion"
+                  >
+                    Reportes de Retroalimentación
+                  </NavDropdown.Item>
+                </NavDropdown>
+
+                <NavDropdown title="Contabilidad" id="accounting-nav-dropdown">
+                  <NavDropdown.Item as={NavLink} to="/ingresos">
+                    Ingresos
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={NavLink} to="/egresos">
+                    Egresos
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={NavLink} to="/reportes-financieros">
+                    Reportes Financieros
+                  </NavDropdown.Item>
+                  <Nav.Link as={NavLink} to="/calendario">
+                    Disponibilidad Citas
+                  </Nav.Link>
+                </NavDropdown>
+
+                <Nav.Link as={NavLink} to="/comentarios-valoraciones">
+                  Opiniones
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="/inventarios">
+                  Inventarios
+                </Nav.Link>
+
+                <NavDropdown title={user.nombre} id="user-nav-dropdown">
+                  <NavDropdown.Item as={NavLink} to="/Perfil">
+                    Perfil
+                  </NavDropdown.Item>
+                  <NavDropdown.Item onClick={handleLogout}>
+                    Cerrar Sesión
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item disabled>Rol: {user.rol}</NavDropdown.Item>
+                </NavDropdown>
+              </>
             ) : (
               <Nav.Link as={NavLink} to="/Inicio">
                 Inicio Sesión
               </Nav.Link>
             )}
 
-            <Nav.Link as={NavLink} to="/Perfil">
-              <FontAwesomeIcon icon={faUser} className="profile-icon" />
-            </Nav.Link>
+            {isAuthenticated && (
+              <Nav.Link as={NavLink} to="/Perfil">
+                <FontAwesomeIcon icon={faUser} className="profile-icon" />
+              </Nav.Link>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
